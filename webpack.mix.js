4e7f460818187e68a39b95ea8e43387c24e20049
Noel Rivas <noel@matrushka.com.mx>
2022-11-20 19:20:30 -0600

    Enabled browser sync, upgraded to PIXI v7 and enabled the production of source maps in Mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
 var mix = require('laravel-mix');
+let productionSourceMaps = true;
 
-mix.js('src/app.js', 'dist').setPublicPath('dist');
+// REMEMBER:
+// To user browser sync, first start a web server.
+// You can use 
+// python3 -m http.server
+mix.browserSync({proxy: {target: 'http://localhost:8000'}, port: 8000});
+
+mix.js('src/app.js', 'dist').setPublicPath('dist')
+  .sourceMaps(productionSourceMaps, 'source-map');
